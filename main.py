955656cfec710f9615ba1abf3bf5a5ecd71cc662
gioprana89 <gioprana89@gmail.com>
2023-03-16 19:24:38 +0700

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import streamlit as st 
 import pandas as pd
 
-col1, col2, col3 = st.columns(3)
+
+import plotly.graph_objects as go
+
+import plotly.express as px
+
+
+
+
+
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Tentang", "Data", "Visualisasi Data", "Analisis Statistik", "Hasil Artikel di Jurnal Nasional/Internasional"])
+
+with tab1:
+   st.header("Tentang")
+tab1.markdown("<h5 style='text-align: justify; color: blue;'>Hadirnya website ini bertujuan untuk menyimpan, mengumpulkan, menyajikan dan melakukan analisis statistik, pada data kinerja keuangan perusahaan yang terdaftar di Bursa Efek Indonesia (BEI).</h5>", unsafe_allow_html=True)
+tab1.image("ugi.jpg", caption='Prana Ugiana Gio', width = 500)
 
 
 
@@ -11,17 +25,48 @@ col1, col2, col3 = st.columns(3)
 
 
 
-
-
-#st.header(":blue[Repositori Data Kinerja Keuangan Perusahaan Indonesia (Bursa Efek Indonesia)]")
-
-st.markdown("<h1 style='text-align: center; color: blue;'>Repositori Data Kinerja Keuangan Perusahaan Indonesia (Bursa Efek Indonesia)</h1>", unsafe_allow_html=True)
-
-st.image("ugi.jpg", caption='Prana Ugiana Gio', width = 500)
-
-
-#st.markdown("<h2 style='text-align: center; color: black;'>Smaller headline in black </h2>", unsafe_allow_html=True)
-
+with tab2:
+   st.header("Data")
 df = pd.read_excel('data_gabungan_seluruh_sektor.xlsx', index_col=0) 
+tab2.dataframe(df, use_container_width=True )
 
-st.dataframe(df, use_container_width=True)
+
+
+
+
+
+
+
+
+
+
+
+with tab3:
+   st.header("Visualisasi Data")
+df = px.data.iris()
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
+                 size='petal_length', hover_data=['petal_width'])
+tab3.plotly_chart(fig, theme=None, use_container_width=True)
+
+
+
+
+
+
+
+
+
+
+with tab4:
+   st.header("Analisis Statistik")
+
+
+
+
+
+with tab5:
+   st.header("Hasil Artikel di Jurnal Nasional/Internasional")
+
+
+
+
